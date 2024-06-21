d001dea062fcb31c8a682e413ca19a8ed05afd30
Adam Wills <adam@adamwills.com>
2024-06-21 09:06:07 -0400

    Initial commit

---
+++ ninjaforms-stripe-taxes.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:     Ninjaforms Stripe Taxes
+ * Plugin URI:      https://adamwills.dev
+ * Description:     Adds support for taxes in Ninja Forms Stripe add-on.
+ * Author:          Adam Wills
+ * Author URI:      https://adamwills.dev
+ * Text Domain:     ninjaforms-stripe-taxes
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Ninjaforms_Stripe_Taxes
+ */
+
+// Your code starts here.
